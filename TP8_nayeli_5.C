710dcc9fdb93f2c6a20069724b7e0f3098ee059e
Nayeli Cirilo <90116499+nayeli47@users.noreply.github.com>
2023-10-18 17:13:38 -0300

    Add files via upload

---
+++ TP8_nayeli_5.C
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int socio[10];
+    int edad[10];
+    int cantidad = 0;
+
+    printf("Ingrese el número de socio y edad de las personas (0 para salir):\n");
+
+    while (cantidad < 10) {
+        int num_socio, age;
+
+        printf("Número de socio: ");
+        scanf("%d", &num_socio);
+
+        if (num_socio == 0) {
+            break;
+        }
+
+        printf("Edad: ");
+        scanf("%d", &age);
+
+        socio[cantidad] = num_socio;
+        edad[cantidad] = age;
+        cantidad++;
+    }
+
+    // Realizar una ordenación manual de los datos por edades (usando el algoritmo de selección)
+    for (int i = 0; i < cantidad - 1; i++) {
+        for (int j = i + 1; j < cantidad; j++) {
+            if (edad[i] > edad[j]) {
+                // Intercambiar edades
+                int temp_edad = edad[i];
+                edad[i] = edad[j];
+                edad[j] = temp_edad;
+
+                // Intercambiar números de socio correspondientes
+                int temp_socio = socio[i];
+                socio[i] = socio[j];
+                socio[j] = temp_socio;
+            }
+        }
+    }
+
+    // Imprimir la lista ordenada por
