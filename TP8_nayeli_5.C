13e88401cdd24ffe768ca7162a23273f38b30944
Nayeli Cirilo <90116499+nayeli47@users.noreply.github.com>
2023-10-21 16:25:23 -0300

    Update TP8_nayeli_5.C

--- TP8_nayeli_5.C
+++ TP8_nayeli_5.C
@@ -1,45 +1,104 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-    int socio[10];
-    int edad[10];
-    int cantidad = 0;
-
-    printf("Ingrese el número de socio y edad de las personas (0 para salir):\n");
-
-    while (cantidad < 10) {
-        int num_socio, age;
-
-        printf("Número de socio: ");
-        scanf("%d", &num_socio);
-
-        if (num_socio == 0) {
-            break;
-        }
-
-        printf("Edad: ");
-        scanf("%d", &age);
-
-        socio[cantidad] = num_socio;
-        edad[cantidad] = age;
-        cantidad++;
-    }
-
-    // Realizar una ordenación manual de los datos por edades (usando el algoritmo de selección)
-    for (int i = 0; i < cantidad - 1; i++) {
-        for (int j = i + 1; j < cantidad; j++) {
-            if (edad[i] > edad[j]) {
-                // Intercambiar edades
-                int temp_edad = edad[i];
-                edad[i] = edad[j];
-                edad[j] = temp_edad;
-
-                // Intercambiar números de socio correspondientes
-                int temp_socio = socio[i];
-                socio[i] = socio[j];
-                socio[j] = temp_socio;
+int main(int argc, char *argv[])
+{
+    int sorteos[3][6] = {{0}, {0}};
+    int loto[2][43] = {{0}, {0}};
+    srand(time(NULL));
+    int i, j, k, l, numeroIngresado = 0, n, conteo1 = 0, conteo2 = 0, auxiliar;
+    
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 6; j++)
+        {
+            sorteos[i][j] = rand() % 43;
+            auxiliar = sorteos[i][j];
+            for (k = 0; k < j + 1; k++)
+            {
+                if (sorteos[i][j] == sorteos[i][k])
+                {
+                    conteo2++;
+                }
+                if (conteo2 == 2)
+                {
+                    sorteos[i][j] = rand() % 43;
+                    conteo2 = 0;
+                    k = -1;
+                }
             }
         }
     }
-
-    // Imprimir la lista ordenada por
+    
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 6; j++)
+        {
+            loto[0][sorteos[i][j]]++;
+        }
+    }
+    
+    printf("Ingrese un número:\n");
+    scanf("%d", &n);
+    
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 6; j++)
+        {
+            if (n == sorteos[i][j])
+            {
+                conteo1 = conteo1 + 1;
+            }
+        }
+    }
+    
+    for (i = 0; i < 43; i++)
+    {
+        loto[1][i] = i;
+    }
+    
+    for (i = 0; i < 43; i++)
+    {
+        for (j = 0; j < 43; j++)
+        {
+            if (loto[0][j] < loto[0][i])
+            {
+                auxiliar = loto[0][j];
+                loto[0][j] = loto[0][i];
+                loto[0][i] = auxiliar;
+                auxiliar = loto[1][j];
+                loto[1][j] = loto[1][i];
+                loto[1][i] = auxiliar;
+            }
+        }
+    }
+    
+    printf("El sorteo es:\n");
+    for (i = 0; i < 3; i++)
+    {
+        printf("El sorteo %d sus números son:\n", i + 1);
+        for (j = 0; j < 6; j++)
+        {
+            printf("\t%d", sorteos[i][j]);
+        }
+        printf("\n");
+    }
+    
+    if (conteo1 > 0)
+    {
+        printf("El número %d aparece %d veces en los sorteos.\n", n, conteo1);
+    }
+    else
+    {
+        printf("El número %d no aparece en los sorteos.\n", n);
+    }
+    
+    printf("Los números más frecuentes son:\n");
+    for (i = 0; i < 5; i++)
+    {
+        printf("Número: %d - Apariciones: %d\n", loto[1][i], loto[0][i]);
+    }
+    
+    return 0;
+}
